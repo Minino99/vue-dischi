18e1a4943d9accb98c44ccc10dd953193392558f
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-24 15:57:28 +0200

    inital commit with header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <TheHeader/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from './components/TheHeader.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {TheHeader,}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+#app{
+  height: 100vh;
+  background-color: #1E2D3B;
+  }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+ <div class="container-fluid px-4 py-3">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="" srcset="">
+ </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  components: {}
+}
+</script>
+
+<style scoped lang="scss">
+.container-fluid{
+ height: 10vh;
+ background-color: #2E3A46;
+
+ img{
+  height: 100%;
+ }
+}
+
+</style>
