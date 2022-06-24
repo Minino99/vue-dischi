a58ef42566b03ac21c9a9de0be9b82b6030f4936
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-24 17:37:13 +0200

    problemi con $emit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-  <TheHeader/>
+  <TheHeader fetchGenre/>
   <AlbumCards/>
 </div>
 </template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,27 +1,52 @@
 <template>
-<div>
- <div class="container-fluid px-4 py-3">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="logo_spotify" srcset="">
- </div>
-</div>
+  <div>
+    <div
+      class="container-fluid px-4 py-3 d-flex justify-content-between align-items-center"
+    >
+      <div class="h-100">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
+          alt="logo_spotify"
+          srcset=""
+        />
+      </div>
+      <div class="d-flex gap-4">
+        <select class="form-select" v-model="selectedGenre">
+          <option value="rock">Rock</option>
+          <option value="pop">Pop</option>
+          <option value="jazz">Jazz</option>
+          <option value="metal">Metal</option>
+        </select>
+        <button type="button" class="btn btn-outline-success" @click="$emit('selectedGenre')">Filtra per genere</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TheHeader',
-  components: {}
-}
+  name: "TheHeader",
+  components: {},
+  data: function () {
+    return {
+      selectedGenre: "",
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.container-fluid{
- height: 10vh;
- min-height: 65px;
- background-color: #2E3A46;
+.container-fluid {
+  height: 10vh;
+  min-height: 65px;
+  background-color: #2e3a46;
 
- img{
-  height: 100%;
- }
+  img {
+    height: 100%;
+  }
+
+  button{
+   white-space: nowrap;
+  }
 }
-
 </style>
